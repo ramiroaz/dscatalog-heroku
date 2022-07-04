c0bc615dccf0d46fd0ca1dd581203fba00518065
Ramiro <ramiroaz@gmail.com>
2022-07-04 17:29:51 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,14 @@
+import Navbar from './components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <h1>Olá DSCatalog</h1>
-  );
+    <>
+      <Navbar />
+      <h1>Olá DSCatalog</h1>
+    </>
+  ); 
 }
 
 export default App;
